a727dcbe1dd15a8f818e40c834d30c242979d890
Rayan Tabora <superguy723@Rayans-MacBook-Pro.local>
2022-07-06 23:29:40 -0400

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,75 @@
+"use strict";
+
+function ownKeys(object, enumerableOnly) {
+  var keys = Object.keys(object);
+  if (Object.getOwnPropertySymbols) {
+    var symbols = Object.getOwnPropertySymbols(object);
+    if (enumerableOnly) symbols = symbols.filter(function (sym) {
+      return Object.getOwnPropertyDescriptor(object, sym).enumerable;
+    });
+    keys.push.apply(keys, symbols);
+  }
+  return keys;
+}
+
+function _objectSpread(target) {
+  for (var i = 1; i < arguments.length; i++) {
+    var source = arguments[i] != null ? arguments[i] : {};
+    if (i % 2) {
+      ownKeys(Object(source), true).forEach(function (key) {
+        _defineProperty(target, key, source[key]);
+      });
+    } else if (Object.getOwnPropertyDescriptors) {
+      Object.defineProperties(target, Object.getOwnPropertyDescriptors(source));
+    } else {
+      ownKeys(Object(source)).forEach(function (key) {
+        Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key));
+      });
+    }
+  }
+  return target;
+}
+
+function _defineProperty(obj, key, value) {
+  if (key in obj) {
+    Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });
+  } else {
+    obj[key] = value;
+  } 
+  return obj;
+}
+
+// Utils
+var docReady = function docReady(fn) {
+  // See If DOM Is Already Avaliable
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', fn);
+  } else {
+    setTimeout(fn, 1);
+  }
+};
+
+var resize = function resize(fn) {
+  return window.addEventListener('resize', fn);
+};
+
+var isIterableArray = function isIterableArray(array) {
+  return Array.isArray(array) && !!array.length;
+};
+
+var camelize = function camelize(str) {
+  var text = str.replace(/[-_\s.]+(.)?/g, function (_, c) {
+    return c ? c.toUpperCase() : '';
+  });
+  return "".concat(text.substr(0, 1).toLowerCase()).concat(text.substr(1));
+};
+
+var getData = function getData(el, data) {
+  try {
+    return JSON.parse(el.dataset[camelize(data)]);
+  } catch (e) {
+    return el.dataset[camelize(data)];
+  }
+};
+
+// Colors Function
